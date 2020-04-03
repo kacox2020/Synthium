0f473bd5148b4e0faf85b1dc14f0e120a205b7d2
kacox2020 <kcox18@bw.edu>
2020-04-03 11:44:47 -0400

    Started Implementation of Basic Views
    Created bottom view navigation but still has errors but prolly due to a bad fragment.

--- Android/app/src/main/java/com/example/synthium/MainActivity.java
+++ Android/app/src/main/java/com/example/synthium/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.synthium;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.homeFragment:
+                    mTextMessage.setText("HOME");
+                    return true;
+                case R.id.musicFragment:
+                    mTextMessage.setText("MUSIC");
+                    return true;
+            }
+            return false;
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
