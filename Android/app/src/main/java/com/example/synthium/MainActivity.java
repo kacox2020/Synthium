05ac6c1b4e1e28118f1a0424c657fa7697b27047
KingKoopa23JR <dylan.johnson1223@gmail.com>
2020-04-13 10:53:08 -0400

    Recycler

--- Android/app/src/main/java/com/example/synthium/MainActivity.java
+++ Android/app/src/main/java/com/example/synthium/MainActivity.java
@@ -3,11 +3,18 @@ package com.example.synthium;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.synthium.dummy.DummyContent;
+
+public class MainActivity extends AppCompatActivity implements MusicFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
