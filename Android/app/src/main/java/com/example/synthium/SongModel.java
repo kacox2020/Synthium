3e31dcf2fbe36d3a7749d417629aadf3b676de69
KingKoopa23JR <dylan.johnson1223@gmail.com>
2020-04-23 17:09:58 -0400

    Merge fix
    for some reason when I merged it deleted a few changes, so I added it back. I just do not understand GitHub

--- Android/app/src/main/java/com/example/synthium/SongModel.java
+++ Android/app/src/main/java/com/example/synthium/SongModel.java
@@ -1,15 +1,19 @@
 package com.example.synthium;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class SongModel {
-
+    List<Song> songs = new ArrayList<>();
     public List<Song> getMusic() {
-        return null;
+        return songs;
     }
 
-    public Song setMusic(){
-        return null;
+    public void setMusic(JSONObject songObject) throws JSONException {
+        Song song = new Song(songObject.getString("songTitle"), songObject.getString("songArtist"), songObject.getInt("songID"), songObject.getInt("songLength"));
+        songs.add(song);
     }
 }
