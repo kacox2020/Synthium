989bafd54f8a28f860449be5b75eef397e183a8c
kacox2020 <kcox18@bw.edu>
2020-05-02 12:13:31 -0400

    Reworked Removed Cleanup Files

--- WebService/rest.php
+++ WebService/rest.php
@@ -1,6 +1,4 @@
 <?php
-require_once(__DIR__ . "/brokers/StockBroker.php");
-require_once(__DIR__ . "/brokers/NoteBroker.php");
 require_once(__DIR__ . "/brokers/SongBroker.php");
 require_once(__DIR__ . "/brokers/SongPlaylistRelationshipBroker.php");
 require_once(__DIR__ . "/brokers/PlaylistBroker.php");
