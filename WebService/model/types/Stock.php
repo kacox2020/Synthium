ab74e78aa7788e529b8094daa5e80fa67f68128d
kacox2020 <kcox18@bw.edu>
2020-05-02 12:04:11 -0400

    Removed unnecessary files

--- WebService/model/types/Stock.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once(__DIR__ . "/Base.php");
-class Stock extends Base {
-  public $symbol, $price;
-  public function __construct($sourceObject) {
-    parent::__construct($sourceObject);
-  }
-}
--- WebService/readme.txt
+++
@@ -1,1 +0,0 @@
-Added a folder for webservices for any webservice files
